1204c840e4268f2db37101c25cf5e1497a04d2f3
Sean Pheneger <wcolts2000@gmail.com>
2019-01-05 00:27:27 -0800

    added main login signup landing page

--- warbler-client/src/containers/App.js
+++ warbler-client/src/containers/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Provider} from "react-redux";
 import {configureStore} from "../store";
 import Navbar from "./Navbar"
+import Main from "./Main";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const store = configureStore()
@@ -11,6 +12,7 @@ const App = () => (
         <Router>
           <div className="onboarding">
             <Navbar />
+            <Main />
           </div>
         </Router>
       </Provider>
---
+++ warbler-client/src/containers/Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Switch, Route, withRouter, Redirect } from "react-router-dom";
+import {connect} from "react-redux";
+import Homepage from "../components/Homepage";
+
+const Main = (props) => {
+  return (
+    <div className="container">
+      <Switch>
+          <Route exact path="/" render={props => <Homepage {...props} />} />
+      </Switch>
+    </div>
+  )
+}
+const mapStateToProps = ({ currentUser }) => {
+    return {
+        currentUser
+    }
+}
+
+export default withRouter(connect(mapStateToProps, null)(Main));
